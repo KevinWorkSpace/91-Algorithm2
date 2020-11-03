66f845a9c7bb7d9ed963cee5ba700545ab096cf0
KevinWorkSpace <11612431@mail.sustc.edu.cn>
2020-11-03 17:01:26 +0800

    finish day 3

---
+++ src/Stack/lecture/InorderTraversal.java
@@ -0,0 +1,30 @@
+package Stack.lecture;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class InorderTraversal {
+
+    private List<Integer> list = new ArrayList<>();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) return list;
+        inorderTraversal(root.left);
+        list.add(root.val);
+        inorderTraversal(root.right);
+        return list;
+    }
+}
