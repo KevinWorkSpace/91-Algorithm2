fc0a9cd1e05e184c61373dd36df433dfacd3a9d8
KevinWorkSpace <11612431@mail.sustc.edu.cn>
2020-11-18 22:12:50 +0800

    finish day 18

--- src/Tree/test/Codec.java
+++ src/Tree/test/Codec.java
@@ -4,11 +4,12 @@ public class Codec {
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        
+
+        return "";
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-
+        return new TreeNode(23);
     }
 }
