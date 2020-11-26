6f2ef0a5c28fa8fc57a891f518073568169d47ac
KevinWorkSpace <11612431@mail.sustc.edu.cn>
2020-11-26 20:17:44 +0800

    finish day 26

---
+++ src/DoublePointer/lecture/SearchRange.java
@@ -0,0 +1,60 @@
+package DoublePointer.lecture;
+
+public class SearchRange {
+
+    public int[] searchRange(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int[] res = new int[2];
+        if (nums.length == 0) {
+            res[0] = -1;
+            res[1] = -1;
+            return res;
+        }
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            }
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid;
+            }
+        }
+        if (nums[left] == target) {
+            res[0] = left;
+        }
+        else res[0] = -1;
+        left = 0;
+        right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left + 1) / 2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            }
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                left = mid;
+            }
+        }
+        if (left == nums.length) {
+            res[1] = -1;
+        }
+        else if (nums[left] == target) {
+            res[1] = left;
+        }
+        else res[1] = -1;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 2};
+        int target = 3;
+        SearchRange searchRange = new SearchRange();
+        int[] res = searchRange.searchRange(nums, target);
+    }
+}
---
+++ src/DoublePointer/test/SearchMatrix.java
@@ -0,0 +1,42 @@
+package DoublePointer.test;
+
+public class SearchMatrix {
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0 || matrix[0].length == 0) return false;
+        int up = 0;
+        int down = matrix.length - 1;
+        while (up < down) {
+            int mid = up + (down - up + 1) / 2;
+            if (matrix[mid][0] <= target) {
+                up = mid;
+            }
+            else {
+                down = mid - 1;
+            }
+        }
+        int left = 0;
+        int right = matrix[up].length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (matrix[up][mid] < target) {
+                left = mid + 1;
+            }
+            else if (matrix[up][mid] > target) {
+                right = mid - 1;
+            }
+            else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] nums = {{1}};
+        int target = 1;
+        SearchMatrix searchMatrix = new SearchMatrix();
+        boolean res = searchMatrix.searchMatrix(nums, target);
+        System.out.println(res);
+    }
+}
