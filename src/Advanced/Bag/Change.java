86c23b0b9caacc0adef6e1463905c4d275fbf90d
KevinWorkSpace <11612431@mail.sustc.edu.cn>
2021-01-29 09:41:49 +0800

    finish day 83

---
+++ src/Advanced/Bag/Change.java
@@ -0,0 +1,16 @@
+package Advanced.Bag;
+
+public class Change {
+
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i=0; i<=amount; i++) {
+                if (i < coin) continue;
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+}
--- src/Advanced/Bag/CoinChange2.java
+++ src/Advanced/Bag/CoinChange2.java
@@ -7,13 +7,9 @@ public class CoinChange2 {
     public int coinChange(int[] coins, int amount) {
         if (amount == 0) return 0;
         int[] dp = new int[amount + 1];
-        int c = coins[0];
         Arrays.fill(dp, amount+1);
-        for (int i=0; i<=amount; i++) {
-            if (c * i > amount) break;
-            dp[c * i] = i;
-        }
-        for (int i=1; i<coins.length; i++) {
+        dp[0] = 0;
+        for (int i=0; i<coins.length; i++) {
             for (int j=0; j<=amount; j++) {
                 if (j - coins[i] >= 0) {
                     dp[j] = Math.min(dp[j], dp[j-coins[i]] + 1);
